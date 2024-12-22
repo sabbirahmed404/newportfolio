b5fb5cd3e1cbc1d6011c356fdcdc8fce48119ff7
Rinkit Adhana <therinkit@gmail.com>
2024-12-22 15:45:21 +0530

    info card mid complete

--- src/components/InfoCard.tsx
+++ src/components/InfoCard.tsx
@@ -1,25 +1,24 @@
 import { gruzBio, gruzImage, gruzLink, gruzName } from "@/data/GruzData/data"
-import Image from "next/image"
 import React from "react"
 const InfoCard: React.FC = () => {
   return (
-    <section>
-      <div className=" flex gap-1">
-        <div>
-          <Image
-            src={gruzImage}
-            alt="Profile Picture"
-            width={100}
-            height={100}
-          />
+    <section className=" py-10">
+      <div className=" flex gap-3 items-center">
+        <div className=" select-none">
+          <img src={gruzImage} alt="Profile Picture" className="pro-pic" />
         </div>
-        <div className=" flex flex-col gap">
-          <h1>{gruzName}</h1>
-          <p>{gruzBio}</p>
-          <div className=" flex flex-wrap gap-1">
+        <div className=" flex flex-col gap-1.5">
+          <h1 className=" head-name ">{gruzName}</h1>
+          <p className="">{gruzBio}</p>
+          <div className=" flex flex-wrap gap-1.5">
             {gruzLink.map((link) => (
-              <a href={link.link} target="_blank" key={link.name}>
-                {link.name}
+              <a
+                className="social-card"
+                key={link.name}
+                href={link.link}
+                target="_blank"
+              >
+                {<link.icon />}
               </a>
             ))}
           </div>
--- src/data/GruzData/data.ts
+++ src/data/GruzData/data.ts
@@ -1,3 +1,6 @@
+import { FaXTwitter, FaGithub, FaDiscord } from "react-icons/fa6"
+import { SiBuymeacoffee } from "react-icons/si"
+
 const gruzImage = "/assets/Images/pfps/gruz.jpeg"
 const gruzName = "Gruz"
 const gruzBio = "Software Engineer"
@@ -5,26 +8,23 @@ const gruzLink = [
   {
     name: "Github",
     link: "https://github.com",
+    icon: FaGithub,
+  },
+
+  {
+    name: "X",
+    link: "https://x.com/intent/follow?screen_name=damnGruz",
+    icon: FaXTwitter,
   },
   {
-    name: "Github",
-    link: "https://github.com",
+    name: "Discord",
+    link: "https://discord.gg/JgcDzHpWum",
+    icon: FaDiscord,
   },
   {
-    name: "Github",
+    name: "BuyMeACoffee",
     link: "https://github.com",
-  },
-  {
-    name: "Github",
-    link: "https://github.com",
-  },
-  {
-    name: "Github",
-    link: "https://github.com",
-  },
-  {
-    name: "Github",
-    link: "https://github.com",
+    icon: SiBuymeacoffee,
   },
 ]
 

843c4a972e654f3f6a8e5f96b463189b6c8e857f
Rinkit Adhana <therinkit@gmail.com>
2024-12-22 16:58:28 +0530

    Info card Updated

--- src/components/InfoCard.tsx
+++ src/components/InfoCard.tsx
@@ -1,5 +1,6 @@
 import { gruzBio, gruzImage, gruzLink, gruzName } from "@/data/GruzData/data"
 import React from "react"
+import { InfoTip } from "./InfoTip"
 const InfoCard: React.FC = () => {
   return (
     <section className=" py-10">
@@ -7,19 +8,22 @@ const InfoCard: React.FC = () => {
         <div className=" select-none">
           <img src={gruzImage} alt="Profile Picture" className="pro-pic" />
         </div>
+
         <div className=" flex flex-col gap-1.5">
           <h1 className=" head-name ">{gruzName}</h1>
           <p className="">{gruzBio}</p>
           <div className=" flex flex-wrap gap-1.5">
             {gruzLink.map((link) => (
-              <a
-                className="social-card"
-                key={link.name}
-                href={link.link}
-                target="_blank"
-              >
-                {<link.icon />}
-              </a>
+              <InfoTip text={link.name}>
+                <a
+                  className="social-card"
+                  key={link.name}
+                  href={link.link}
+                  target="_blank"
+                >
+                  {<link.icon />}
+                </a>
+              </InfoTip>
             ))}
           </div>
         </div>

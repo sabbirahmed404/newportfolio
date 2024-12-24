2ce48d605ee01f4185d867aeab17683dc071fbe9
Rinkit Adhana <therinkit@gmail.com>
2024-12-25 00:12:44 +0530

    section title update

--- src/components/SectionTitle.tsx
+++ src/components/SectionTitle.tsx
@@ -1,7 +1,7 @@
 import React from "react"
 
 const SectionTitle = ({ title }: { title: string }) => {
-  return <h1 className=" text-lg font-semibold">{title}</h1>
+  return <h1 className=" text-lg font-semibold">{title}.</h1>
 }
 
 export default SectionTitle

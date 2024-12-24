b7693249561ae129da90bb678c3148445167d114
Rinkit Adhana <therinkit@gmail.com>
2024-12-25 00:06:58 +0530

    blog box done

--- src/components/BlogBox.tsx
+++ src/components/BlogBox.tsx
@@ -1,12 +1,11 @@
-import { gruzImage } from "@/data/GruzData/data"
 import React from "react"
 
 const BlogBox = () => {
   return (
     <div className=" blog-post flex  items-center gap-2">
       <img
-        className="size-16 rounded-md"
-        src={gruzImage}
+        className="size-[68px] object-cover select-none rounded-md"
+        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS5Q3Hv7-rg8dfuKPn-r79jjVJEQaeg7g17mA&s"
         alt="Profile Picture"
       />
       <div className="flex flex-col ">
@@ -14,7 +13,7 @@ const BlogBox = () => {
           A Step-by-Step Guide to Advanced Task Scheduling using Celery Beat and
           Django
         </h1>
-        <p className=" text-xs text-zinc-400">
+        <p className=" text-sm text-zinc-400">
           Let me take you back to when the world seemed upside down during the
           late 2020s, with the coronavirus causing chaos everywhere. At 17, I
           dipped my toes into the world of coding

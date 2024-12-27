f71377070c0a62d62092f0ed7ef3cc816dfa0f23
Rinkit Adhana <therinkit@gmail.com>
2024-12-27 21:56:11 +0530

    project box enhance

--- src/components/ProjectBox.tsx
+++ src/components/ProjectBox.tsx
@@ -29,10 +29,14 @@ const ProjectBox: React.FC<ProjectBoxProps> = ({
   preview,
 }) => {
   const [show, setShow] = useState(false)
+  const [open, setOpen] = useState(false)
 
   return (
-    <section className=" group/show group/pi">
-      <div className=" bg-zinc-900 border border-zinc-700 rounded-md">
+    <div
+      onClick={() => setOpen((prev) => !prev)}
+      className=" group/show group/pi"
+    >
+      <div className=" cursor-pointer bg-zinc-900 hover:bg-zinc-800/75 transition-colors duration-100  border border-zinc-700 rounded-md">
         {show && (
           <div className="p-2">
             <video className=" rounded-md" width="900" controls>
@@ -126,38 +130,41 @@ const ProjectBox: React.FC<ProjectBoxProps> = ({
             ))}
           </div>
         </div>
-
-        <div className="md:hidden flex md:group-hover/show:flex border-t border-zinc-700 w-[97%] my-1 mx-auto" />
-        <div className="    md:hidden flex md:group-hover/show:flex md:justify-between justify-end items-center p-2 transition-all duration-100">
-          <div className=" hidden md:flex flex-wrap gap-1.5 select-none">
-            {skill.map((skill, index) => (
-              <p
-                key={index}
-                className=" border border-zinc-700 px-2 py-0.5  rounded-md text-sm"
-              >
-                {skill}
-              </p>
-            ))}
-          </div>
-          <div className="flex gap-4 items-center px-4 text-lg ">
-            <div className=" cursor-pointer hover:text-pink-500 transition-colors duration-100 select-none flex  gap-1 items-center  ">
-              <div>
-                <FaRegHeart />
+        {open && (
+          <>
+            <div className="flex border-t border-zinc-700 w-[97%] my-1 mx-auto" />
+            <div className="flex md:justify-between justify-end items-center p-2 transition-all duration-100">
+              <div className=" hidden md:flex flex-wrap gap-1.5 select-none">
+                {skill.map((skill, index) => (
+                  <p
+                    key={index}
+                    className=" border border-zinc-700 px-2 py-0.5  rounded-md text-sm"
+                  >
+                    {skill}
+                  </p>
+                ))}
               </div>
-              <div className=" text-sm">134</div>
+              <div className="flex gap-4 items-center px-4 text-lg ">
+                <div className=" cursor-pointer hover:text-pink-500 transition-colors duration-100 select-none flex  gap-1 items-center  ">
+                  <div>
+                    <FaRegHeart />
+                  </div>
+                  <div className=" text-sm">134</div>
+                </div>
+                <InfoTipProjects text="Share">
+                  <div
+                    className="cursor-pointer hover:text-zinc-400 transition-colors duration-100"
+                    onClick={() => navigator.share({ url: url ? url : github })}
+                  >
+                    <LuShare />
+                  </div>
+                </InfoTipProjects>
+              </div>
             </div>
-            <InfoTipProjects text="Share">
-              <div
-                className="cursor-pointer hover:text-zinc-400 transition-colors duration-100"
-                onClick={() => navigator.share({ url: url ? url : github })}
-              >
-                <LuShare />
-              </div>
-            </InfoTipProjects>
-          </div>
-        </div>
+          </>
+        )}
       </div>
-    </section>
+    </div>
   )
 }
 

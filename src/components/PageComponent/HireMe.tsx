ef1f341a0081295295572d389064c4b8ee99f42a
Rinkit Adhana <therinkit@gmail.com>
2025-01-08 14:14:34 +0530

    hire me in both profile

--- src/components/PageComponent/HireMe.tsx
+++ src/components/PageComponent/HireMe.tsx
@@ -5,26 +5,19 @@ import { emailLink, hireText } from "@/data/Common/data"
 import { useSwitch } from "../Context/SwitchContext"
 
 const HireMe = () => {
-  const { isSwitchOn } = useSwitch()
   return (
-    <>
-      {isSwitchOn ? (
-        <></>
-      ) : (
-        <section className="flex flex-col gap-0.5">
-          <SectionTitle title="Hire Me" />
-          <div className=" flex flex-col gap-2">
-            <p>{hireText}</p>
-            <div>
-              <a className="btn" target="_blank" href={emailLink}>
-                <PiSuitcaseSimpleBold />
-                Hire Me
-              </a>
-            </div>
-          </div>
-        </section>
-      )}
-    </>
+    <section className="flex flex-col gap-0.5">
+      <SectionTitle title="Hire Me" />
+      <div className=" flex flex-col gap-2">
+        <p>{hireText}</p>
+        <div>
+          <a className="btn" target="_blank" href={emailLink}>
+            <PiSuitcaseSimpleBold />
+            Hire Me
+          </a>
+        </div>
+      </div>
+    </section>
   )
 }
 

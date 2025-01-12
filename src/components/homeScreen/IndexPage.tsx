90bd7a4ee1f16e308c3b029ead59c6bc9b0fca9a
Rinkit Adhana <therinkit@gmail.com>
2025-01-12 22:21:09 +0530

    animation added

--- src/components/homeScreen/IndexPage.tsx
+++ src/components/homeScreen/IndexPage.tsx
@@ -7,19 +7,34 @@ import Skills from "../PageComponent/Skills"
 import ReachOut from "../PageComponent/ReachOut"
 import Writings from "../PageComponent/Writings"
 import HireMe from "../PageComponent/HireMe"
+import AnimatedWrapper from "@/utils/AnimatedWrapper"
 
 const IndexPage = () => {
   return (
     <>
       <Screen>
         <div className="flex flex-col gap-5">
-          <InfoCard />
-          <AboutMe />
-          <HireMe />
-          <Skills />
-          <Projects />
-          <Writings />
-          <ReachOut />
+          <AnimatedWrapper threshold={0.1} delay={0}>
+            <InfoCard />
+          </AnimatedWrapper>
+          <AnimatedWrapper threshold={0.1} delay={0.15}>
+            <AboutMe />
+          </AnimatedWrapper>
+          <AnimatedWrapper threshold={0.1} delay={0.25}>
+            <HireMe />
+          </AnimatedWrapper>
+          <AnimatedWrapper threshold={0.1} delay={0.35}>
+            <Skills />
+          </AnimatedWrapper>
+          <AnimatedWrapper threshold={0.1} delay={0.45}>
+            <Projects />
+          </AnimatedWrapper>
+          <AnimatedWrapper threshold={0.1} delay={0.55}>
+            <Writings />
+          </AnimatedWrapper>
+          <AnimatedWrapper threshold={0.1} delay={0.65}>
+            <ReachOut />
+          </AnimatedWrapper>
         </div>
         <Footer />
       </Screen>

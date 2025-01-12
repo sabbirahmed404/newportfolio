2b41ba9eb336fed2a282fe4d76bc95278064d6d1
Rinkit Adhana <therinkit@gmail.com>
2025-01-13 04:24:30 +0530

    Refactor AnimatedWrapper: Clean up code by removing unused imports and simplifying component structure

--- src/utils/AnimatedWrapper.tsx
+++ src/utils/AnimatedWrapper.tsx
@@ -1,26 +1,22 @@
 "use client"
 
 import React from "react"
-import { motion, useInView } from "framer-motion"
+import { motion } from "framer-motion"
 import { useSwitch } from "@/components/Context/SwitchContext"
 
 interface AnimatedWrapperProps {
   children: React.ReactNode
   delay?: number
-  
 }
 
-const AnimatedWrapper = ({
-  children,
-  delay = 0, 
-}: AnimatedWrapperProps) => {
+const AnimatedWrapper = ({ children, delay = 0 }: AnimatedWrapperProps) => {
   const { isSwitchOn } = useSwitch()
-  
+
   return (
     <motion.div
       key={String(isSwitchOn)}
       initial={{ opacity: 0, y: -20 }}
-      animate={ { opacity: 1, y: 0 }}
+      animate={{ opacity: 1, y: 0 }}
       transition={{
         duration: 0.35,
         delay: delay,

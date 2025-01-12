c0cecb7ce60f3c2ff5d1138bfe30166234d374df
Rinkit Adhana <therinkit@gmail.com>
2025-01-13 04:26:04 +0530

    animation direction

--- src/utils/AnimatedWrapper.tsx
+++ src/utils/AnimatedWrapper.tsx
@@ -15,7 +15,7 @@ const AnimatedWrapper = ({ children, delay = 0 }: AnimatedWrapperProps) => {
   return (
     <motion.div
       key={String(isSwitchOn)}
-      initial={{ opacity: 0, y: -20 }}
+      initial={{ opacity: 0, y: 20 }}
       animate={{ opacity: 1, y: 0 }}
       transition={{
         duration: 0.35,

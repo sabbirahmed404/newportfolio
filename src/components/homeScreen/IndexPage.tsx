72a6eeaba4460b30f31f142f7e4b6e8219e0502c
Rinkit Adhana <therinkit@gmail.com>
2025-01-13 03:35:37 +0530

    added animation comp to index page

--- src/components/homeScreen/IndexPage.tsx
+++ src/components/homeScreen/IndexPage.tsx
@@ -14,25 +14,21 @@ const IndexPage = () => {
     <>
       <Screen>
         <div className="flex flex-col gap-5">
-          <AnimatedWrapper threshold={0.1} delay={0}>
+          <AnimatedWrapper threshold={0.5} delay={0}>
             <InfoCard />
           </AnimatedWrapper>
-          <AnimatedWrapper threshold={0.1} delay={0.15}>
+          <AnimatedWrapper threshold={0.5} delay={0.15}>
             <AboutMe />
           </AnimatedWrapper>
-          <AnimatedWrapper threshold={0.1} delay={0.25}>
+          <AnimatedWrapper threshold={0.5} delay={0.25}>
             <HireMe />
           </AnimatedWrapper>
-          <AnimatedWrapper threshold={0.1} delay={0.35}>
+          <AnimatedWrapper threshold={0.5} delay={0.35}>
             <Skills />
           </AnimatedWrapper>
-          <AnimatedWrapper threshold={0.1} delay={0.45}>
-            <Projects />
-          </AnimatedWrapper>
-          <AnimatedWrapper threshold={0.1} delay={0.55}>
-            <Writings />
-          </AnimatedWrapper>
-          <AnimatedWrapper threshold={0.1} delay={0.65}>
+          <Projects />
+          <Writings />
+          <AnimatedWrapper threshold={0.5} delay={0.45}>
             <ReachOut />
           </AnimatedWrapper>
         </div>

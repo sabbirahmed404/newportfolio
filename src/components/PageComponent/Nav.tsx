8b0d1c986120f5daec94976525a1b219dc1712aa
Rinkit Adhana <therinkit@gmail.com>
2025-02-18 13:14:52 +0530

    navbar enhanced

--- src/components/PageComponent/Nav.tsx
+++ src/components/PageComponent/Nav.tsx
@@ -1,8 +1,9 @@
-import { gruzImage, gruzName } from "@/data/GruzData/data"
+import { gruzImage } from "@/data/GruzData/data"
 import { useSwitch } from "../Context/SwitchContext"
-import { rinkitImage, rinkitShortName } from "@/data/RinkitData/data"
+import { rinkitImage } from "@/data/RinkitData/data"
 import Image from "next/image"
 import { navLinks } from "@/data/Common/data"
+import { InfoTipNav } from "../InfoTipNav"
 
 const Nav = () => {
   const { isSwitchOn, toggleSwitch } = useSwitch()
@@ -10,22 +11,18 @@ const Nav = () => {
   return (
     <div className="fixed bottom-6 left-0 right-0 flex justify-center items-center">
       <div className="bg-mainBlack border border-zinc-700 px-3 py-2 rounded-xl flex items-center gap-3">
-        <div className=" flex gap-0.5 items-center">
+        <div className=" flex gap-4 items-center">
           {navLinks.map((nav) => (
-            <a
-              key={nav.id}
-              title={nav.name}
-              target="_blank"
-              className="  p-2 hover:bg-zinc-800 transition duration-100 rounded-md"
-              href={nav.link}
-            >
-              {<nav.icon />}
-            </a>
+            <InfoTipNav key={nav.id} text={nav.name}>
+              <a target="_blank" className=" " href={nav.link}>
+                {<nav.icon />}
+              </a>
+            </InfoTipNav>
           ))}
         </div>
         <div className="h-8 w-[1px] bg-zinc-500  mr-1"></div>
         <div
-          className="rounded-md hover:brightness-75 cursor-pointer"
+          className="rounded-md hover:brightness-75 transition duration-200 cursor-pointer"
           onClick={toggleSwitch}
         >
           <Image
@@ -34,7 +31,7 @@ const Nav = () => {
             className="rounded-md"
             width={35}
             height={35}
-            title={isSwitchOn ? rinkitShortName : gruzName}
+            // title={isSwitchOn ? rinkitShortName : gruzName}
           />
         </div>
       </div>

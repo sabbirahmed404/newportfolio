c5be3e50764ef3a7fbf9f3a5dda533b2d6761bf3
Rinkit Adhana <therinkit@gmail.com>
2025-06-08 04:43:19 +0530

    star button updated

--- src/components/StarButton.tsx
+++ src/components/StarButton.tsx
@@ -1,14 +1,28 @@
-import React from "react"
+import React, { useState } from "react"
+import { FaRegStar, FaStar } from "react-icons/fa6"
 
 const StarButton = () => {
+  const [click, setClick] = useState(false)
+  const handleClick = () => {
+    setClick(true)
+  }
   return (
     <a
-      className=" hover:underline text-white/75 hover:text-yellow-400 font-medium"
+      className="px-4 py-1.5 border border-zinc-700 flex items-center gap-2 rounded-[9px] bg-zinc-900 hover:bg-zinc-900/60 transition-all duration-200"
       target="_blank"
+      onClick={handleClick}
       href="https://github.com/rinkitadhana/Switchfolio"
     >
-      <span className="hidden md:block text-sm">Star on GitHub</span>
-      <span className="md:hidden">Star</span>
+      {click ? (
+        <FaStar className="size-[17px] text-yellow-300" />
+      ) : (
+        <FaRegStar className="size-[17px]" />
+      )}
+
+      <span className="text-sm">{click ? "Starred" : "Star"}</span>
+      <span className="bg-zinc-800 rounded-xl px-3 py-0.5 text-xs">
+        {click ? "15" : "14"}
+      </span>
     </a>
   )
 }

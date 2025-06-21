3c4baef7d0e099c2ed9deeac0fb9bc0b38229c9f
Rinkit Adhana <therinkit@gmail.com>
2025-06-22 02:52:59 +0530

    Add api to start button to track real time data

--- src/components/StarButton.tsx
+++ src/components/StarButton.tsx
@@ -1,27 +1,61 @@
-import React, { useState } from "react"
-import { FaRegStar, FaStar } from "react-icons/fa6"
+import React, { useEffect, useState } from "react"
+import { AiOutlineLoading3Quarters } from "react-icons/ai"
+import { FaRegStar } from "react-icons/fa6"
+
+interface StarData {
+  stargazers_count: number
+  name: string
+  full_name: string
+}
 
 const StarButton = () => {
-  const [click, setClick] = useState(false)
-  const handleClick = () => {
-    setClick(true)
-  }
+  const [stars, setStars] = useState<number | null>(null)
+  const [loading, setLoading] = useState(true)
+  const [error, setError] = useState<string | null>(null)
+
+  useEffect(() => {
+    const fetchStars = async () => {
+      try {
+        const response = await fetch(
+          `https://api.github.com/repos/rinkitadhana/switchfolio`,
+          {
+            headers: {
+              Accept: "application/vnd.github.v3+json",
+            },
+          }
+        )
+
+        if (!response.ok) {
+          throw new Error(`Error: ${response.status}`)
+        }
+
+        const data: StarData = await response.json()
+        setStars(data.stargazers_count)
+      } catch (err) {
+        setError(err instanceof Error ? err.message : "Failed to fetch")
+      } finally {
+        setLoading(false)
+      }
+    }
+    fetchStars()
+  }, [])
+
   return (
     <a
       className="px-3 py-1.5 border border-zinc-700 hidden md:flex items-center gap-2 rounded-[9px] bg-zinc-900 hover:bg-zinc-900/60 transition-all duration-200"
       target="_blank"
-      onClick={handleClick}
       href="https://github.com/rinkitadhana/Switchfolio"
     >
-      {click ? (
-        <FaStar className="size-[17px] text-yellow-300" />
-      ) : (
-        <FaRegStar className="size-[17px]" />
-      )}
-
-      <span className="text-sm">{click ? "Starred" : "Star"}</span>
+      <FaRegStar className="size-[17px]" />
+      <span className="text-sm">{"Star"}</span>
       <span className="bg-zinc-800 rounded-xl px-3 py-0.5 text-xs font-medium">
-        {click ? "15" : "14"}
+        {error ? (
+          "16"
+        ) : loading ? (
+          <AiOutlineLoading3Quarters className="animate-spin size-[16px] p-0.5" />
+        ) : (
+          stars
+        )}
       </span>
     </a>
   )

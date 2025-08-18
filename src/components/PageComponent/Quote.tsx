a54480d30cca4357ea1b7a5f785fc815c60549cd
Rinkit Adhana <therinkit@gmail.com>
2025-08-18 16:57:54 +0530

    lil fix

--- src/components/PageComponent/Quote.tsx
+++ src/components/PageComponent/Quote.tsx
@@ -3,7 +3,7 @@ const Quote = () => {
 
     return (
         <section className="w-full my-3">
-            <div className=" w-full text-center opacity-60 italic">&quot;I might not be the best developer, but I&apos;m the best developer&quot;</div>
+            <div className=" w-full text-center opacity-60 italic text-sm">&quot;I might not be the best developer, but I&apos;m the best developer&quot;</div>
         </section>
     )
 }
